d2fc080951ab0a276bee467b494e93e6d10ba738
Vladimir Valyukh <vlava115@student.liu.se>
2012-12-02 22:20:37 +0100

    modified:   install.sh 	new file:   update.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,31 @@
 echo Check Internet Connection!
 
 # Read Password
-echo -n Password: 
-read -s password
-echo
+#echo -n Password: 
+#read -s password
+#echo
+
+passwd
 
 #update
 echo Updating..
 apt-get update
 apt-get dist-upgrade -y
 
-echo Updating Metasploit
+echo Updating Metasploit..
 msfupdate
 
 #install private dir  and make links
+
+
+ecryptfs-setup-private
+ecryptfs-mount-private
+
+cd /root/Private/
+git clone https://github.com/vlad-sensei/BackTrack.git
+cd /root/
+
+
 
 #add to bash.rc PATH and move bash.history to private dir
 
