9088fb1e2d63328e7046a8a16ae017b1c05ac0f7
Vladimir Valyukh <vlava115@student.liu.se>
2012-12-03 15:13:10 +0100

    modified:   link.sh

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 BTD='/root/Private/BackTrack'
 
 ln -s /root/Private/.bash_history .bash_history
-ln -s $BTD/rc/bashrc .bashrc
+ln -s $BTD/rc/bashrc /root/.bashrc
 ln -s $BTD/dt /root/Desktop
+ln -s $BTD /x
 
