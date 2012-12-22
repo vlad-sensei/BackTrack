3d7ef398ed565f65ec6ed736f870a42aa46e4f67
Vladimir Valyukh <vladimir.sensei@gmail.com>
2012-12-22 22:35:48 +0100

    modified:   sc/x.py

--- sc/x.py
+++ sc/x.py
@@ -4,4 +4,4 @@ import datetime, os.path
 def makeloot(s):
   d='/x/loot/' + s + '_' + str(datetime.datetime.now()).split('.')[0].replace(' ','_') + '/'
   if not os.path.exists(d): os.makedirs(d)
-  return dir
+  return d
