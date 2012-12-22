27a592b30d35ea1a5302f8a18445d63a32d20ce6
Vladimir Valyukh <vladimir.sensei@gmail.com>
2012-12-22 22:21:58 +0100

    modified:   amanda 	modified:   sslstrip/ClientRequest.pyc 	modified:   sslstrip/CookieCleaner.pyc 	modified:   sslstrip/DnsCache.pyc 	modified:   sslstrip/SSLServerConnection.pyc 	modified:   sslstrip/ServerConnection.pyc 	modified:   sslstrip/ServerConnectionFactory.pyc 	modified:   sslstrip/StrippingProxy.pyc 	modified:   sslstrip/URLMonitor.pyc 	modified:   sslstrip/__init__.pyc 	new file:   x.py

---
+++ sc/x.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import datetime, os.path
+
+def makeloot(s):
+  d='/x/loot/' + s + '_' + str(datetime.datetime.now()).split('.')[0].replace(' ','_') + '/'
+  if not os.path.exists(d): os.path.makedirs(d)
+  return dir
